12740cdbde798a7bee113f6dd02def0cb06437ab
padisoftbiz <128324024+padisoftbiz@users.noreply.github.com>
2023-05-08 19:45:33 -0700

    Update cpVolumes-deleteDuples.py

--- cpVolumes-deleteDuples.py
+++ cpVolumes-deleteDuples.py
@@ -17,7 +17,10 @@ Usage:
     Set the paths for source, destination, log file, delete script, and skip list file in the main() function.
     Run the script to copy files, generate the delete script, and log actions.
 """
-
+"""
+Tool for copying files from one directory to another while avoiding duplicates, skipping hidden files/folders and
+files specified in a skip list, and generating a script to delete duplicate files on the target directory.
+"""
 
 import fnmatch
 import json
@@ -25,25 +28,105 @@ import os
 import shutil
 import hashlib
 import shlex
-from pathlib import Path
 import concurrent.futures
 import threading
+from pathlib import Path
+
+import time
+import threading
+from threading import Event
 
 
-def process_file(src, dst, log, skip_list, progress, progress_lock, root, file):
-    """
-    Process a file, calculating its hash and copying it if it is not a duplicate.
+HASHMAP_UPDATE_INTERVAL = 300  # Save the hashmap every 5 minutes (300 seconds)
 
-    Args:
-        src (str): Path to the source directory.
-        dst (str): Path to the destination directory.
-        log (file): A file object for the log file.
-        skip_list (set): A set of file paths to be skipped.
-        progress (dict): A dictionary containing the progress of the file copy.
-        progress_lock (Lock): A lock to ensure thread safety when accessing the progress dictionary.
-        root (str): The root directory for the file being processed.
-        file (str): The name of the file being processed.
-    """
+def generate_target_hashmap(dst, max_workers=500):
+    target_hashmap = {}
+    hashmap_lock = threading.Lock()
+
+    def process_target_file(target_file):
+        file_hash = hash_file(target_file)
+        with hashmap_lock:
+            target_hashmap[target_file] = file_hash
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
+        futures = []
+        for root, _, files in os.walk(dst):
+            for file in files:
+                target_file = os.path.join(root, file)
+                futures.append(executor.submit(process_target_file, target_file))
+        concurrent.futures.wait(futures)
+
+    return target_hashmap
+
+HASHMAP_UPDATE_THRESHOLD = 10
+
+def save_target_hashmap(target_hashmap, hashmap_file, stop_event):
+    update_counter = 0
+    while not stop_event.is_set():
+        time.sleep(HASHMAP_UPDATE_INTERVAL)
+        with target_hashmap_lock:
+            if update_counter >= HASHMAP_UPDATE_THRESHOLD:
+                with open(hashmap_file, "w") as f:
+                    target_hashmap_copy = target_hashmap.copy()
+                    json.dump(target_hashmap_copy, f)
+                update_counter = 0
+            else:
+                update_counter += 1
+
+
+def copy_files(src, dst, log_path, skip_list_path, max_workers=500):
+    # Load skip list
+    with open(skip_list_path, "r") as f:
+        skip_list = f.read().splitlines()
+
+    # Check for progress file and load it
+    progress_file = "progress.json"
+    progress = {}
+    if os.path.exists(progress_file):
+        with open(progress_file, "r") as f:
+            progress = json.load(f)
+
+    progress_lock = threading.Lock()
+
+    # Check for hashmap file and load it, or generate a new one
+    hashmap_file = "target_hashmap.json"
+    if os.path.exists(hashmap_file):
+        with open(hashmap_file, "r") as f:
+            target_hashmap = json.load(f)
+    else:
+        with open(log_path, "a") as log:
+            log.write("Generating target directory hashmap...\n")
+        target_hashmap = generate_target_hashmap(dst)
+        with open(log_path, "a") as log:
+            log.write("Target directory hashmap generated.\n")
+        with open(hashmap_file, "w") as f:
+            json.dump(target_hashmap, f)
+
+    # Start a separate thread to save the hashmap periodically
+    stop_event = Event()
+    hashmap_saving_thread = threading.Thread(target=save_target_hashmap, args=(target_hashmap, hashmap_file, stop_event))
+    hashmap_saving_thread.start()
+
+    # Open the log file for appending
+    with open(log_path, "a") as log:
+        with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
+            for root, _, files in os.walk(src):
+                # Process files concurrently
+                futures = [executor.submit(process_file, src, dst, log, skip_list, progress, progress_lock, target_hashmap, root, file)
+                           for file in files]
+                concurrent.futures.wait(futures)
+
+    stop_event.set()
+    hashmap_saving_thread.join()
+
+
+FILE_UPDATE_THRESHOLD = 10
+
+file_counter = 0
+file_counter_lock = threading.Lock()
+
+def process_file(src, dst, log, skip_list, progress, progress_lock, target_hashmap, root, file):
+    global file_counter
     source_file = os.path.join(root, file)
     target_file = os.path.join(dst, os.path.relpath(source_file, src))
 
@@ -59,6 +142,9 @@ def process_file(src, dst, log, skip_list, progress, progress_lock, root, file):
     # Calculate the file hash and check for duplicates
     file_hash = hash_file(source_file)
 
+    # Check if the target file already exists and has the same hash using the hashmap
+    if target_file in target_hashmap and file_hash == target_hashmap[target_file]:
+        return
     with progress_lock:
         if file_hash in progress.values():
             log.write(f"DUPLICATE: {source_file}\n")
@@ -67,34 +153,16 @@ def process_file(src, dst, log, skip_list, progress, progress_lock, root, file):
             shutil.copy(source_file, target_file)
             log.write(f"COPIED: {source_file} -> {target_file}\n")
             progress[source_file] = file_hash
+            with target_hashmap_lock:
+                target_hashmap[target_file] = file_hash  # Update the target_hashmap after copying the file
 
-            # Save progress
-            with open(progress_file, "w") as f:
-                json.dump(progress, f)
-
-
-def copy_files(src, dst, log_path, skip_list_path, max_workers=10):
-    # Load skip list
-    with open(skip_list_path, "r") as f:
-        skip_list = f.read().splitlines()
-
-    # Check for progress file and load it
-    progress_file = "progress.json"
-    progress = {}
-    if os.path.exists(progress_file):
-        with open(progress_file, "r") as f:
-            progress = json.load(f)
-
-    progress_lock = threading.Lock()
-
-    # Open the log file for appending
-    with open(log_path, "a") as log:
-        with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
-            for root, _, files in os.walk(src):
-                # Process files concurrently
-                futures = [executor.submit(process_file, src, dst, log, skip_list, progress, progress_lock, root, file)
-                           for file in files]
-                concurrent.futures.wait(futures)
+        # Increment the file counter here, outside the else block
+        with file_counter_lock:
+            file_counter += 1
+            if file_counter % FILE_UPDATE_THRESHOLD == 0:
+                with open(progress_file, "w") as f:
+                    json.dump(progress, f)
+                log.flush()
 
 
 def hash_file(file_path):
@@ -113,7 +181,6 @@ def hash_file(file_path):
             hasher.update(chunk)
     return hasher.hexdigest()
 
-
 def read_skip_list(skip_list_path):
     """
     Read the skip list file and return a set of file paths to be skipped.
@@ -128,17 +195,12 @@ def read_skip_list(skip_list_path):
         skip_list = {line.strip() for line in skip_list_file}
     return skip_list
 
-
 def delete_duplicates(dst, delete_script_path):
     """
     Generate a shell script to delete duplicate files in the destination directory.
-
     Args:
         dst (str): Path to the destination directory.
         delete_script_path (str): Path to the delete script file.
-
-    Returns:
-        None
     """
     file_hashes = {}
     with open(delete_script_path, "w") as delete_script:
@@ -152,44 +214,27 @@ def delete_duplicates(dst, delete_script_path):
                     file_hashes[file_hash] = target_file
                 else:
                     delete_script.write(f"rm {shlex.quote(target_file)}\n")
-
-
 def log_actions(log_path):
     """
-    Print the contents of the log file to the console.
-
+    Print the contents of the log file.
     Args:
         log_path (str): Path to the log file.
-
-    Returns:
-        None
     """
     with open(log_path, "r") as log_file:
         print(log_file.read())
 
-
 def main():
-    """
-    Main function to execute the script.
-
-    Set the paths for source, destination, log file,
-    delete script, and skip list. Copy files, generate delete script, and log actions.
-
-    Returns:
-        None
-    """
     src = "/Volumes/My Passport"
     dst = "/Volumes/NO NAME"
     log_path = "file_transfer_log.txt"
     delete_script_path = "delete_duplicates.sh"
     skip_list_path = "skip_list.txt"
-    max_workers = 100
 
     skip_list = read_skip_list(skip_list_path)
-    copy_files(src, dst, log_path, skip_list_path, max_workers=max_workers)
+    copy_files(src, dst, log_path, skip_list_path)
     delete_duplicates(dst, delete_script_path)
     log_actions(log_path)
 
-
 if __name__ == "__main__":
+    target_hashmap_lock = threading.Lock()
     main()
